51e92b11f415dc23d2a02cce6c68386320c41fe3
Luigi-Crisci <luigicrisci1997@gmail.com>
2024-07-18 15:48:30 +0000

    Improve prof_i kernel dependencies

--- src/prof_i.cc
+++ src/prof_i.cc
@@ -1,11 +1,12 @@
-#include <prof_i.hh>
 #include <file_manager.hh>
+#include <prof_i.hh>
 
 #include <sycl/sycl.hpp>
 
 namespace bgk {
 
-static inline constexpr char format_1002[sizeof("{:14.6e} {:14.6e} {:14.6e} {:14.6e}\n")] = "{:14.6e} {:14.6e} {:14.6e} {:14.6e}\n";
+static inline constexpr char format_1002[sizeof("{:14.6e} {:14.6e} {:14.6e} {:14.6e}\n")]
+    = "{:14.6e} {:14.6e} {:14.6e} {:14.6e}\n";
 static inline constexpr char format_1005[sizeof("# t={:7d}\n")] = "# t={:7d}\n";
 
 void prof_i(storage &bgk_storage, const int itime, const int jcoord) {
@@ -22,85 +23,75 @@ void prof_i(storage &bgk_storage, const int itime, const int jcoord) {
 #endif
 
     // Density calculation
-    auto& q = *bgk_storage.host_q;
+    auto &q = *bgk_storage.host_q;
 
     // for(int i = 0; i < bgk_storage.l; ++i) {
-    //     den[i] = (bgk_storage.a01_host(i,jcoord) + bgk_storage.a03_host(i,jcoord) + bgk_storage.a05_host(i,jcoord) + bgk_storage.a08_host(i,jcoord) + bgk_storage.a10(i,jcoord) + bgk_storage.a12_host(i,jcoord)
+    //     den[i] = (bgk_storage.a01_host(i,jcoord) + bgk_storage.a03_host(i,jcoord) + bgk_storage.a05_host(i,jcoord) +
+    //     bgk_storage.a08_host(i,jcoord) + bgk_storage.a10(i,jcoord) + bgk_storage.a12_host(i,jcoord)
     //                  + bgk_storage.a14_host(i,jcoord) + bgk_storage.a17(i,jcoord) + bgk_storage.a19(i,jcoord))
     //         + cte1;
     // }
-    
-    q.parallel_for(sycl::range<1>{static_cast<size_t>(bgk_storage.l)}, [
-    den = den.data(),
-    jcoord,
-    cte1,
-    a01 = bgk_storage.a01_host,
-    a03 = bgk_storage.a03_host,
-    a05 = bgk_storage.a05_host,
-    a08 = bgk_storage.a08_host,
-    a10 = bgk_storage.a10_host,
-    a12 = bgk_storage.a12_host,
-    a14 = bgk_storage.a14_host,
-    a17 = bgk_storage.a17_host,
-    a19 = bgk_storage.a19_host
-    ](sycl::item<1> idx) {
-        const auto i = idx.get_linear_id() + 1;
-        den[i] = (a01(i,jcoord) + a03(i,jcoord) + a05(i,jcoord) + a08(i,jcoord) + a10(i,jcoord) + a12(i,jcoord)
-                     + a14(i,jcoord) + a17(i,jcoord) + a19(i,jcoord))
-            + cte1;
-    }).wait_and_throw();
 
-    
+    auto event = q.parallel_for(sycl::range<1>{static_cast<size_t>(bgk_storage.l)},
+        [den = den.data(), jcoord, cte1, a01 = bgk_storage.a01_host, a03 = bgk_storage.a03_host,
+            a05 = bgk_storage.a05_host, a08 = bgk_storage.a08_host, a10 = bgk_storage.a10_host,
+            a12 = bgk_storage.a12_host, a14 = bgk_storage.a14_host, a17 = bgk_storage.a17_host,
+            a19 = bgk_storage.a19_host](sycl::item<1> idx) {
+            const auto i = idx.get_linear_id() + 1;
+            den[i] = (a01(i, jcoord) + a03(i, jcoord) + a05(i, jcoord) + a08(i, jcoord) + a10(i, jcoord)
+                         + a12(i, jcoord) + a14(i, jcoord) + a17(i, jcoord) + a19(i, jcoord))
+                + cte1;
+        });
+
+
     // Streamwise velocity calculation
     // for(int i = 0; i < bgk_storage.l; ++i) {
-    //     u[i] = (bgk_storage.a01_host(i,jcoord) + bgk_storage.a03_host(i,jcoord) + bgk_storage.a05_host(i,jcoord) - bgk_storage.a10_host(i,jcoord) - bgk_storage.a12(i,jcoord) - bgk_storage.a14(i,jcoord))
+    //     u[i] = (bgk_storage.a01_host(i,jcoord) + bgk_storage.a03_host(i,jcoord) + bgk_storage.a05_host(i,jcoord) -
+    //     bgk_storage.a10_host(i,jcoord) - bgk_storage.a12(i,jcoord) - bgk_storage.a14(i,jcoord))
     //         / den[i];
     // }
     // Streamwise velocity calculation
-    q.parallel_for(sycl::range<1>{static_cast<size_t>(bgk_storage.l)}, [
-    u = u.data(),
-    jcoord,
-    den = den.data(),
-    a01 = bgk_storage.a01_host,
-    a03 = bgk_storage.a03_host,
-    a05 = bgk_storage.a05_host,
-    a10 = bgk_storage.a10_host,
-    a12 = bgk_storage.a12_host,
-    a14 = bgk_storage.a14_host
-    ](sycl::item<1> idx) {
-        const auto i = idx.get_linear_id() + 1;
-        u[i] = (a01(i,jcoord) + a03(i,jcoord) + a05(i,jcoord) - a10(i,jcoord) - a12(i,jcoord) - a14(i,jcoord))
-            / den[i];
-    }).wait_and_throw();
+    event = q.submit([&](sycl::handler &cgh) {
+        cgh.depends_on(event);
 
+        cgh.parallel_for(sycl::range<1>{static_cast<size_t>(bgk_storage.l)},
+            [u = u.data(), jcoord, den = den.data(), a01 = bgk_storage.a01_host, a03 = bgk_storage.a03_host,
+                a05 = bgk_storage.a05_host, a10 = bgk_storage.a10_host, a12 = bgk_storage.a12_host,
+                a14 = bgk_storage.a14_host](sycl::item<1> idx) {
+                const auto i = idx.get_linear_id() + 1;
+                u[i] = (a01(i, jcoord) + a03(i, jcoord) + a05(i, jcoord) - a10(i, jcoord) - a12(i, jcoord)
+                           - a14(i, jcoord))
+                    / den[i];
+            });
+    });
 
     // Normal-to-wall velocity calculation
     // for(int i = 0; i < bgk_storage.l; ++i) {
-    //     v[i] = (bgk_storage.a03_host(i,jcoord) + bgk_storage.a08_host(i,jcoord) + bgk_storage.a12_host(i,jcoord) - bgk_storage.a01_host(i,jcoord) - bgk_storage.a10(i,jcoord) - bgk_storage.a17(i,jcoord))
+    //     v[i] = (bgk_storage.a03_host(i,jcoord) + bgk_storage.a08_host(i,jcoord) + bgk_storage.a12_host(i,jcoord) -
+    //     bgk_storage.a01_host(i,jcoord) - bgk_storage.a10(i,jcoord) - bgk_storage.a17(i,jcoord))
     //         / den[i];
     // }
-    q.parallel_for(sycl::range<1>{static_cast<size_t>(bgk_storage.l)}, [
-    v = v.data(),
-    jcoord,
-    den = den.data(),
-    a01 = bgk_storage.a01_host,
-    a03 = bgk_storage.a03_host,
-    a08 = bgk_storage.a08_host,
-    a10 = bgk_storage.a10_host,
-    a12 = bgk_storage.a12_host,
-    a17 = bgk_storage.a17_host
-    ](sycl::item<1> idx) {
-        const auto i = idx.get_linear_id() + 1;
-        v[i] = (a03(i,jcoord) + a08(i,jcoord) + a12(i,jcoord) - a01(i,jcoord) - a10(i,jcoord) - a17(i,jcoord))
-            / den[i];
-    }).wait_and_throw();
+    event = q.submit([&](sycl::handler& cgh){
+        cgh.depends_on(event);
+    cgh.parallel_for(sycl::range<1>{static_cast<size_t>(bgk_storage.l)},
+         [v = v.data(), jcoord, den = den.data(), a01 = bgk_storage.a01_host, a03 = bgk_storage.a03_host,
+             a08 = bgk_storage.a08_host, a10 = bgk_storage.a10_host, a12 = bgk_storage.a12_host,
+             a17 = bgk_storage.a17_host](sycl::item<1> idx) {
+             const auto i = idx.get_linear_id() + 1;
+             v[i]
+                 = (a03(i, jcoord) + a08(i, jcoord) + a12(i, jcoord) - a01(i, jcoord) - a10(i, jcoord) - a17(i, jcoord))
+                 / den[i];
+         });
+    });
 
-    auto& file_manager = debug::file_manager::instance();
+    event.wait();
+
+    auto &file_manager = debug::file_manager::instance();
     file_manager.write_format<format_1005>(61, itime);
 
     // Write data
     for(int i = 1; i <= bgk_storage.l; ++i) {
-        file_manager.write_format<format_1002>(61, (i-0.5), u[i], v[i], den[i]);
+        file_manager.write_format<format_1002>(61, (i - 0.5), u[i], v[i], den[i]);
     }
 
     // Write empty lines
@@ -108,8 +99,10 @@ void prof_i(storage &bgk_storage, const int itime, const int jcoord) {
 
 // Debug output
 #ifdef DEBUG_1
-    if(bgk_storage.myrank == 0) { std::cout << "DEBUG1: Exiting from sub. prof_i" << "\n"; }
+    if(bgk_storage.myrank == 0) {
+        std::cout << "DEBUG1: Exiting from sub. prof_i"
+                  << "\n";
+    }
 #endif
-
 }
-}
+} // namespace bgk
