cb2dccaf77caa3f4d31e57dec3516fbe6dbc6bd1
Luigi-Crisci <luigicrisci1997@gmail.com>
2024-07-22 10:20:58 +0200

    Fix boundary conditions arguments + add ndrange variant for channel bcond

--- src/bcond/bcond_channel.cc
+++ src/bcond/bcond_channel.cc
@@ -18,7 +18,9 @@ namespace bgk{
     utils::time(timing.tcountA0);
 
     auto& q = *bgk_storage.dev_q;
-    const auto event = q.submit([&](sycl::handler& cgh){
+
+    #ifndef SYCL_ND_RANGE
+    [[maybe_unused]] const auto event = q.submit([&](sycl::handler& cgh){
         constexpr auto start = 0;
         const auto end = bgk_storage.m + 2; // m + 1 (inclusive)
 
@@ -88,7 +90,83 @@ cgh.depends_on(event);
            a17(i  ,m1) = a08(i,m);
            a01(i-1,m1) = a12(i,m); 
     });
-}).wait_and_throw();
+});
+#else
+constexpr auto local_size = 64;
+
+[[maybe_unused]] const auto event = q.submit([&](sycl::handler& cgh){
+        constexpr auto start = 0;
+        const auto end = bgk_storage.m + 2; // m + 1 (inclusive)
+        cgh.parallel_for(sycl::nd_range<1>({static_cast<size_t>(end)}, {local_size}),
+        [
+            a01 = bgk_storage.a01_device,
+            a03 = bgk_storage.a03_device,
+            a05 = bgk_storage.a05_device,
+            a08 = bgk_storage.a08_device,
+            a10 = bgk_storage.a10_device,
+            a12 = bgk_storage.a12_device,
+            a14 = bgk_storage.a14_device,
+            a17 = bgk_storage.a17_device,
+            l = bgk_storage.l,
+            l1 = bgk_storage.l1
+        ](sycl::nd_item<1> id){
+            const auto j = id.get_global_id(0);
+        //
+        // rear, periodic bc  (x = l)
+        //
+           a10(l1,j) = a10(1,j);
+           a12(l1,j) = a12(1,j);
+           a14(l1,j) = a14(1,j);
+        //
+        // -------------------------------------------------------------
+        // front, periodic (x = 0)
+        //           
+           a01( 0,j) = a01(l,j);
+           a03( 0,j) = a03(l,j);
+           a05( 0,j) = a05(l,j);
+        });
+    });
+
+//
+// ----------------------------------------------
+// left (y = 0)  
+// right (y = m) 
+// ----------------------------------------------
+//
+
+q.submit([&](sycl::handler& cgh){
+    #ifndef SYCL_IN_ORDER_QUEUE
+cgh.depends_on(event);
+#endif
+    constexpr auto start = 0;
+    const auto end = bgk_storage.l + 2; // l + 1 (inclusive)
+
+    cgh.parallel_for(sycl::nd_range<1>({static_cast<size_t>(end)}, {local_size}),
+    [
+        a01 = bgk_storage.a01_device,
+        a03 = bgk_storage.a03_device,
+        a08 = bgk_storage.a08_device,
+        a10 = bgk_storage.a10_device,
+        a12 = bgk_storage.a12_device,
+        a17 = bgk_storage.a17_device,
+        m = bgk_storage.m,
+        m1 = bgk_storage.m1
+    ](sycl::nd_item<1> id){
+        const auto i = id.get_global_id(0);
+        // left, noslip  (y = 0)  
+           a08(i  ,0)  = a17(i,1);
+           a12(i+1,0)  = a01(i,1);
+           a03(i-1,0)  = a10(i,1);
+
+        // right, noslip  (y = m) 
+           a10(i+1,m1) = a03(i,m);
+           a17(i  ,m1) = a08(i,m);
+           a01(i-1,m1) = a12(i,m); 
+    });
+});
+#endif
+
+q.wait();
 
 //Stop timing
 utils::time(timing.tcountA1);
--- src/bcond/boundaries.cc
+++ src/bcond/boundaries.cc
@@ -1,3 +1,4 @@
+#include "bcond/bcond_channel.hh"
 #include <bcond/boundaries.hh>
 #include <bcond/bcond_driven.hh>
 #include <bcond/bcond_obs.hh>
@@ -11,16 +12,16 @@ void boundaries(storage &bgk_storage) {
     // Different boundary conditions (selected at compile-time)
 #ifdef PERIODIC
     // Periodic boundary condition
-    bcond_periodic();
+    bcond_periodic(bgk_storage);
 #elif CHANNEL
     // Channel boundary condition
-    bcond_channel();
+    bcond_channel(bgk_storage);
 #elif INFLOW
     // Inflow boundary condition
-    bcond_inflow();
+    bcond_inflow(bgk_storage);
 #elif MYVERSION
     // Periodic boundary condition (my version)
-    bcond_periodic();
+    bcond_periodic(bgk_storage);
 #else
     // Lid-driven cavity boundary condition (default)
     bcond_driven(bgk_storage);
